632ca4533043f95fdb18248df815250d2ca4aaf4
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-03-17 22:38:06 +0530

    Update product.java

--- spring-boot-API/src/main/java/com/example/springbootAPI/model/Product.java
+++ spring-boot-API/src/main/java/com/example/springbootAPI/model/Product.java
@@ -1,4 +1,22 @@
 package com.example.springbootAPI.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int product_id;
+    private String category_id;
+    private String product_brand;
+    private String product_name;
+    private double product_price;
+    private int product_quantity;
 }
